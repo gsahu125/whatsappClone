fe0d962ce9eaae251238eb1e69fde0675e5846b2
Gajendra <gsahu125@gmail.com>
2021-05-31 18:19:47 +0530

    project initiated with required dependency

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'react-native-gesture-handler'
+import {View,Text} from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>This is app component</Text>
+    </View>
+  );
+};
+
+export default App;
